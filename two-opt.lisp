f69b8bc3ca6bcfeb81e976543db12ddbf6784efb
phil8192 <phil@parasec.net>
2014-05-29 18:44:07 +0300

    start cleanup

--- two-opt.lisp
+++ two-opt.lisp
@@ -20,23 +20,17 @@
 (defmacro point-active (point)
   `(aref ,point 2))
 
-;;(declaim (inline distance-squared))
 (defun distance-squared (p1 p2)
   "for comparing 2 edges."
-;;  (declare (optimize (speed 3) (debug 0) (safety 0))
-;;	   (type vec3 p1 p2))
   (let ((dx (- (point-x p1) (point-x p2)))
 	(dy (- (point-y p1) (point-y p2))))
     (+ (* dx dx) (* dy dy))))
 
 (defun distance (p1 p2)
-;;  (declare (optimize (speed 3) (debug 0) (safety 0))
-;;	   (type vec3 p1 p2))
   "euclidean distance."
   (sqrt (distance-squared p1 p2)))
 
 (defun tour-distance (points)
-;;  (declare (optimize (speed 3) (debug 0) (safety 0)))
   (let ((tour-length (length points)))
     (do ((i 1 (1+ i))
 	 (d 0 (+ d (distance (aref points (1- i)) (aref points i)))))
@@ -70,7 +64,6 @@ iterated in order, represents a tour."
 		:initial-contents (reverse result))))
 
 (defun reverse-subseq (array from to)
-;;  (declare (type fixnum from to))
   (do ((i from (1+ i))
        (j to (1- j)))
       ((>= i j))
@@ -78,35 +71,22 @@ iterated in order, represents a tour."
       (setf (svref array i) (svref array j))
       (setf (svref array j) tmp))))
 
-;;(declaim (inline wrap))
 (defun wrap (i max)
-;;  (declare (optimize (speed 3) (debug 0) (safety 0))
-;;	   (type fixnum i max))
   (the fixnum (mod (the fixnum (+ i max)) max)))
 
-;;(declaim (inline move-cost))
 (defun move-cost (a b c d)
-;;  (declare (type vec3 a b c d)
-;;	   (optimize (speed 3) (debug 0) (safety 0)))
   (let ((ab (distance-squared a b)) (cd (distance-squared c d))
 	(ac (distance-squared a c)) (bd (distance-squared b d)))
-;;    (declare (type double-float ab cd ac bd))
     (if (and (< ab ac) (< cd bd))
 	1.0d0
 	(- (+ (sqrt ac) (sqrt bd))
            (+ (sqrt ab) (sqrt cd))))))
 
 (defun activate (a b c d)
-;;  (declare (type vec3 a b c d)
-;;	   (optimize (speed 3) (debug 0) (safety 0)))
   (setf (point-active a) 1.0d0) (setf (point-active b) 1.0d0)
   (setf (point-active c) 1.0d0) (setf (point-active d) 1.0d0))
 
-;;(declaim (inline try-move))
 (defun try-move (points from to a b c d)
-;;  (declare (type simple-array points)
-;;	   (type fixnum from to)
-;;	   (type vec3 a b c d))
   (let ((delta (move-cost a b c d)))
     (if (< delta 0.0d0)
 	(progn 
@@ -117,10 +97,6 @@ iterated in order, represents a tour."
 
 (defun find-move (current-idx current-point
 		  points num-cities)
-;;  (declare (type fixnum current-idx num-cities)
-;;	   (type vec3 current-point)
-;;	   (type simple-array points)
-;;	   (optimize (speed 3) (debug 0) (safety 0)))
   (let* ((prev (wrap (1- current-idx) num-cities))
 	 (next (wrap (1+ current-idx) num-cities))
 	 (prev-point (svref points prev))
@@ -138,21 +114,15 @@ iterated in order, represents a tour."
 
 ;; nasty.	
 (defun optimise (points)
-;;  (declare (type simple-array points)
-;;	   (optimize (speed 3) (debug 0) (safety 0)))
   (let ((best (tour-distance points))
 	(num-cities (length points))
 	(visited 0)
 	(current 0))
-;;    (declare (type (double-float 0.0d0 *) best)
-;;	     (type fixnum num-cities visited current)) 
     (do ()
 	((= visited num-cities) best)
       (let ((current-point (svref points current)))
-;;	(declare (type vec3 current-point))
 	(if (= (point-active current-point) 1.0d0)
 	    (let ((modified (find-move current current-point points num-cities)))
-;;	      (declare (type double-float modified))
 	      (if (< modified 0.0d0)
 		  (progn
 		    (setf current (wrap (1- current) num-cities))
